33f7fab8a3e7d17c1bed7c6f4544f8baefc73c8a
Adeen <adeenkhoso@gmail.com>
2024-02-19 22:52:44 +0500

    lp navbar made

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.{html,js}'],
+  theme: {
+    colors:{
+      white:'#FFFFFF',
+      primary_dark:'#203147',
+      primary_light:'#3B5271',
+      secondary_color:'91B0DA',
+      primary_bg:'#DDE6ED',
+      hover_text_color:'color: rgb(148 163 184);'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
