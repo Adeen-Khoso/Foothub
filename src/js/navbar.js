bf0faffce35ca5b3025aa84e9fe84a930f4d825b
Adeen <adeenkhoso@gmail.com>
2024-02-22 20:47:44 +0500

    featured section done

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,9 +1,22 @@
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+const heroSection = document.getElementById('hero-section');
+const textContainer = document.getElementById('text-container');
 
 menuBtn.addEventListener('click', function() {
-    const expanded = this.getAttribute('aria-expanded') === 'true' || false;
-    this.setAttribute('aria-expanded', !expanded);
     menu.classList.toggle('hidden');
     menu.classList.toggle('flex');
 });
+
+window.addEventListener('scroll', function() {
+    let scrolledValue = 70;
+    let scrolled = window.scrollY > scrolledValue;
+  
+    if (scrolled) {
+      nav.classList.add('scrolled');
+    } else {
+      nav.classList.remove('scrolled');
+    }
+  });
+  
